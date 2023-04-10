a733e9ca06bf22b57e4ceec895fb62bdd192838c
Jasoonn02 <jason_0297@outloo.com>
2023-04-10 14:24:23 -0300

    change

--- src/Proyectos/Proyectos.jsx
+++ src/Proyectos/Proyectos.jsx
@@ -5,13 +5,19 @@ import { BsGithub, BsEye } from "react-icons/bs";
 
 export default function Proyectos() {
   return (
-    <section className="my-12 flex flex-col items-center justify-center px-8 m-auto lg:flex-row">
+    <div>
+      <div className="font-serif flex justify-center text-3xl mt-6">
+        <h1>
+          Mis Proyectos
+        </h1>
+      </div>
+    <section className="my-12 flex flex-col items-center justify-center px-8 m-auto  lg:flex-row lg:space-x-6">
       <div className="border-1 rounded-lg bg-white">
         <div className="m-2">
           <img src={simple} alt="" />
           <h4 className="font-bold my-2">SIMPLE SERVICE</h4>
           <article className="flex justify-between">
-            <div className="grid grid-cols-2 items-center">
+            <div className="grid grid-cols-2 items-center md:grid-cols-4">
             <span className="text-[12px] bg-slate-200 rounded-lg px-2 mr-2 text-slate-700 my-1">JS</span>
             <span className="text-[12px] bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">REACT.JS</span>
             <span className="text-[12px] bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">REDUX-TOOLKIT</span>
@@ -31,12 +37,12 @@ export default function Proyectos() {
           </article>
         </div>
       </div>
-      <div className="border-1 rounded-lg bg-white mt-6">
+      <div className="border-1 rounded-lg bg-white mt-6 lg:w-[180%] lg:mt-0 xl:w-[82%]">
         <div className="m-2">
           <img src={videogame} alt="" />
           <h4 className="font-bold my-2">VIDEOGAMES</h4>
           <article className="flex justify-between">
-            <div className="grid grid-cols-2 items-center">
+            <div className="grid grid-cols-2 items-center md:grid-cols-4">
             <span className="text-[12px] bg-slate-200 rounded-lg px-2 mr-2 text-slate-700 my-1">JS</span>
             <span className="text-[12px] bg-slate-200 rounded-lg px-2 mr-2 text-slate-700">REACT.JS</span>
             <span className="text-[12px] bg-slate-200 rounded-lg px-2 mr-2 text-slate-700 my-1">REDUX</span>
@@ -56,5 +62,6 @@ export default function Proyectos() {
         </div>
       </div>
     </section>
+    </div>
   );
 }
